f198564afac667f066b6a6e1249ae9b600f289a8
Pascal Hartig <phartig@rdrei.net>
2018-12-04 18:07:39 +0000

    Day 1, Part 1

---
+++ README.md
@@ -0,0 +1,9 @@
+# Advent Of Code 2018
+
+Some solutions (spoilers!) to the [Advent of Code 2018](https://adventofcode.com/2018/day) puzzles.
+I'm not committed to any particular language, but you'll likely see PureScript/Haskell/Rust here
+if I keep on it.
+
+## License
+
+MIT
---
+++ day01/src/Main.purs
@@ -0,0 +1,48 @@
+module Main where
+
+import Prelude
+
+import Data.Foldable (foldl)
+import Data.Int as Int
+import Data.Maybe (Maybe(Nothing))
+import Data.Newtype (traverse)
+import Data.String (CodePoint, uncons)
+import Data.String.CodePoints (codePointFromChar)
+import Data.String.Yarn (lines)
+import Data.Traversable (sequence)
+import Effect (Effect)
+import Effect.Class.Console (logShow)
+import Effect.Console (log)
+import Node.Encoding (Encoding(UTF8))
+import Node.FS.Sync (readTextFile)
+
+data Op = Add Int
+        | Subtract Int
+
+minus :: CodePoint
+minus = codePointFromChar '-'
+
+plus :: CodePoint
+plus = codePointFromChar '+'
+
+parse :: String -> Maybe Op
+parse str = do
+  {head: h, tail: t} <- uncons str
+  parseOp h t
+
+parseOp :: CodePoint -> String -> Maybe Op
+parseOp x xs | x == minus = Subtract <$> Int.fromString xs
+             | x == plus  = Add      <$> Int.fromString xs
+             | otherwise  = Nothing
+
+reduceOp :: Int -> Op -> Int
+reduceOp b (Add a) = b + a
+reduceOp b (Subtract a) = b - a
+
+main :: Effect Unit
+main = do
+  logShow =<< map (foldl reduceOp 0)
+          <$> sequence
+          <$> map parse
+          <$> lines
+          <$> readTextFile UTF8 "input.txt"
