0f665a25b4efad791279d253088a39d4f8be5388
Pascal Hartig <phartig@rdrei.net>
2018-12-04 18:54:41 +0000

    Non-stacksafe possible solution for day 1 part 2

--- day01/src/Main.purs
+++ day01/src/Main.purs
@@ -2,15 +2,19 @@ module Main where
 
 import Prelude
 
+import Data.Either (Either(Left, Right))
 import Data.Foldable (foldl)
+import Data.HashSet (HashSet)
+import Data.HashSet as HashSet
 import Data.Int as Int
-import Data.Maybe (Maybe(Nothing))
+import Data.List.Lazy as List
+import Data.Maybe (Maybe(Nothing), fromMaybe)
 import Data.String (CodePoint, uncons)
 import Data.String.CodePoints (codePointFromChar)
 import Data.String.Yarn (lines)
 import Data.Traversable (sequence)
 import Effect (Effect)
-import Effect.Class.Console (logShow)
+import Effect.Class.Console (log, logShow)
 import Node.Encoding (Encoding(UTF8))
 import Node.FS.Sync (readTextFile)
 
@@ -37,10 +41,43 @@ reduceOp :: Int -> Op -> Int
 reduceOp b (Add a) = b + a
 reduceOp b (Subtract a) = b - a
 
-main :: Effect Unit
-main = do
+part1 :: Effect Unit
+part1 =
   logShow =<< map (foldl reduceOp 0)
           <$> sequence
           <$> map parse
           <$> lines
           <$> readTextFile UTF8 "input.txt"
+
+type ReduceState =
+  { set :: HashSet Int
+  , sum :: Int
+  }
+
+reduceRepeat :: ReduceState -> Op -> Either ReduceState ReduceState
+reduceRepeat {set, sum} op =
+  let sum' = reduceOp sum op
+      set' = HashSet.insert sum' set
+      res  = {set: set', sum: sum'}
+  in
+    if HashSet.member sum' set
+    then Left res
+    else Right res
+
+part2 :: Effect Unit
+part2 = do
+  ops <- fromMaybe []
+      <$> sequence
+      <$> map parse
+      <$> lines
+      <$> readTextFile UTF8 "input.txt"
+
+  let res = List.foldM reduceRepeat { set: HashSet.empty, sum: 0 } $ List.cycle $ List.fromFoldable ops
+  case res of
+    Right _ -> log "I fucked up."
+    Left {sum} -> logShow sum
+
+main :: Effect Unit
+main = do
+  part1
+  part2
